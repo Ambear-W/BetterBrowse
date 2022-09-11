a59737856116b3d30738b262c12b51ac3eb4d197
Amber W <43912805+Ambear-W@users.noreply.github.com>
2022-09-10 20:32:57 -0400

    got the alt tags to work yaay!!!

---
+++ popup/azureCalls.js
@@ -0,0 +1,45 @@
+let betterAltsBtn = document.getElementById("betterAlts")
+betterAltsBtn.addEventListener("click", async() =>{
+    alert("clicked!");
+    let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+            function: () => {
+                function azureVision(imageUrl, img){
+                    alert("in azure vision");
+                    var request = new XMLHttpRequest();
+                    request.open('POST', 'https://betterbrowse.cognitiveservices.azure.com/vision/v1.0/describe?maxCandidates=1&language=en', true);
+                    request.setRequestHeader('Content-Type', 'application/json');
+                    request.setRequestHeader('Ocp-Apim-Subscription-Key', '410023a8c34e447fb3505fcea6a2983c');
+                    request.send(JSON.stringify({ "url": imageUrl }));
+                    request.onload = function () {
+                        var resp = request.responseText;
+                        if (request.status >= 200 && request.status < 400) {
+                            // Success!
+                            console.log('Success!');
+                            img.alt = (JSON.parse(resp)).description.captions[0].text;
+                        } else {
+                            // We reached our target server, but it returned an error
+                            console.error('Error!');
+                        }
+                        console.log(JSON.parse(resp));
+                    };
+                    request.onerror = function (e) {
+                        console.log(e);
+                    };
+                }
+
+                let imgs = document.getElementsByTagName("img");
+                for (var i = 0;i < imgs.length; i++){
+                    imageToSend = imgs[i].src;
+                    try{
+                        azureVision(imageToSend, imgs[i]);
+                    } catch (err){
+                        console.log(err);
+                    }
+                    
+                }
+            }
+    })
+})
+
